b40da2fd7983a09070c8be2c264051445665dfe8
Pedro Santos <pedrohenry.viana@gmail.com>
2023-09-17 13:21:27 -0300

    feat: add plugins in node

--- node.js
+++ node.js
@@ -1,31 +1,39 @@
 module.exports = {
   env: {
-    es2021: true,
+    es2022: true,
     node: true,
+    jest: true,
   },
-  extends: ['standard', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-  parser: '@typescript-eslint/parser',
+  extends: [
+    "standard",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    'plugin:promise/recommended',
+    'plugin:unicorn/recommended',
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ["@typescript-eslint", "eslint-plugin-import-helpers"],
   rules: {
-    'prettier/prettier': [
-      'error',
+    "prettier/prettier": [
+      "error",
       {
         printWidth: 80,
         tabWidth: 2,
         singleQuote: true,
-        trailingComma: 'all',
-        arrowParens: 'always',
+        trailingComma: "all",
+        arrowParens: "always",
         semi: false,
+        endOfLine: "auto",
       },
     ],
   },
   settings: {
-    'import/parsers': {
-      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    "import/parsers": {
+      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
     },
   },
-}
+};
